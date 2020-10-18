e1f692857756b28aa16c63c44ec00a3ac6c7a83f
Syed Saeed Ul Hassan Ali <ssaeed@folio3.com>
2020-10-19 03:42:01 +0500

    Updates

---
+++ precache-manifest.aa161b32560cd355d88fcb2584ab2479.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d5e1a1af06f76e2838dc98e93fcf17b",
+    "url": "/simple-weather-app/index.html"
+  },
+  {
+    "revision": "204e17d25474ea1a54c7",
+    "url": "/simple-weather-app/static/css/2.d9986383.chunk.css"
+  },
+  {
+    "revision": "e8d229822e64687f26c2",
+    "url": "/simple-weather-app/static/css/main.f21a3fdf.chunk.css"
+  },
+  {
+    "revision": "204e17d25474ea1a54c7",
+    "url": "/simple-weather-app/static/js/2.e150cf8e.chunk.js"
+  },
+  {
+    "revision": "a07000321a1ebac9c07126414b759ee0",
+    "url": "/simple-weather-app/static/js/2.e150cf8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8d229822e64687f26c2",
+    "url": "/simple-weather-app/static/js/main.b59a220a.chunk.js"
+  },
+  {
+    "revision": "bdc5a1d6cc8d4afb1777",
+    "url": "/simple-weather-app/static/js/runtime-main.882d2e1c.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/simple-weather-app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/simple-weather-app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/simple-weather-app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/simple-weather-app/static/media/slick.f97e3bbf.svg"
+  }
+]);
